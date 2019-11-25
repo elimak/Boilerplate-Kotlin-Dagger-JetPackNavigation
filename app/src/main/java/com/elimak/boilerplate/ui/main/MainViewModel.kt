7bceb0287c13e5662ebede25e4f9c63367580341
elimak <info@elimak.com>
2019-11-25 15:45:15 -0800

    added repository injection into the viewmodel

--- app/src/main/java/com/elimak/boilerplate/ui/main/MainViewModel.kt
+++ app/src/main/java/com/elimak/boilerplate/ui/main/MainViewModel.kt
@@ -2,7 +2,17 @@ package com.elimak.boilerplate.ui.main
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.elimak.boilerplate.App
+import com.elimak.boilerplate.repository.IRepository
+import javax.inject.Inject
 
 class MainViewModel : ViewModel() {
+    @Inject
+    lateinit var repository: IRepository
+
     val text: ObservableField<String> = ObservableField("Main Fragment Title")
+
+    init {
+        App.instance.getApplicationComponent().inject(this)
+    }
 }
